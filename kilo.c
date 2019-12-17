651ddbfa21387b03a91c703fda1be61527d28af1
Spencer Wolf <spwolf18@gmail.com>
2019-12-17 14:06:57 -0500

    Add sections for git diff

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+/*** includes ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,7 +6,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die (const char *err) {
 	perror(err);
@@ -31,6 +36,8 @@ void enableRawMode() {
 
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
 	enableRawMode();
