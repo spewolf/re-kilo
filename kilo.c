7e28f580e850c14ab9825847ef03ace439662618
Spencer Wolf <spwolf18@gmail.com>
2019-12-23 18:07:40 -0500

    Add DEL key

--- kilo.c
+++ kilo.c
@@ -22,6 +22,7 @@ enum editorKey {
 	ARROW_DOWN,
 	PAGE_UP,
 	PAGE_DOWN,
+	DEL_KEY,
 	HOME_KEY,
 	END_KEY
 };
@@ -95,6 +96,7 @@ int editorReadKey() {
 				if (seq[2] == '~') {
 					switch (seq[1]) {
 						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
 						case '4': return END_KEY;
 						case '5': return PAGE_UP;
 						case '6': return PAGE_DOWN;
