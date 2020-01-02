fb63506c4b1804f5d13f5cbee07b8c919b6df6d1
Spencer Wolf <spwolf18@gmail.com>
2020-01-01 22:40:16 -0500

    Implement save functionality

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -263,6 +264,25 @@ void editorInsertChar(int c) {
 
 /*** file I/O ***/
 
+char *editorRowsToString(int *buflen) {
+	int totlen = 0;
+	int j;
+	for (j = 0; j < config.numrows; j++)
+		totlen += config.row[j].size + 1;
+	*buflen = totlen;
+
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for (j = 0; j < config.numrows; j++) {
+		memcpy(p, config.row[j].chars, config.row[j].size);
+		p += config.row[j].size;
+		*p = '\n';
+		p++;
+	}
+
+	return buf;
+}
+
 void editorOpen(char* filename) {
 	free(config.filename);
 	config.filename = strdup(filename);
@@ -282,6 +302,19 @@ void editorOpen(char* filename) {
 	}
 	free(line);
 	fclose(fp);
+}
+
+void editorSave() {
+	if (config.filename == NULL) return;
+
+	int len;
+	char *buf = editorRowsToString(&len);
+
+	int fd = open(config.filename, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, len);
+	write(fd, buf, len);
+	close(fd);
+	free(buf);
 }
 
 /*** append ***/
@@ -498,6 +531,9 @@ void editorProcessKeypress() {
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case CTRL_KEY('s'):
+			editorSave();
 			break;
 
 		// Move keys
