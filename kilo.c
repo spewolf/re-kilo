21ad110c8055d00004bb3e838cb9d200b986a987
Spencer Wolf <spwolf18@gmail.com>
2020-01-09 15:57:36 -0500

    Implement forward and backward searching

--- kilo.c
+++ kilo.c
@@ -439,17 +439,37 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+	static int last_match = -1;
+	static int direction = 1;
+
 	if (key == '\r' || key == '\x1b') {
+		last_match = -1;
+		direction = 1;
 		return;
+	} else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+		direction = 1;
+	} else if (key == ARROW_LEFT || key == ARROW_UP) {
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
 	}
+
+	if (last_match == -1) direction = 1;
+	int current = last_match;
 
 	// search rows for term
 	int i;
 	for (i = 0; i < config.numrows; i++) {
-		erow *row = &config.row[i];
+		current += direction;
+		if (current == -1) current = config.numrows - 1;
+		else if (current == config.numrows) current = 0;
+
+		erow *row = &config.row[current];
 		char *match = strstr(row->render, query);
 		if (match) {
-			config.cy = i;
+			last_match = current;
+			config.cy = current;
 			config.cx = editorRowRxToCx(row, match - row->render);
 			config.rowoff = config.numrows;
 			break;
