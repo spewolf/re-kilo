40bb417583274e24eadda4734afe328bc7d7a870
Spencer Wolf <spwolf18@gmail.com>
2020-01-09 22:52:59 -0500

    Add search highlighting

--- kilo.c
+++ kilo.c
@@ -41,7 +41,8 @@ enum editorKey {
 
 enum editorHighlight {
 	HL_NORMAL = 0,
-	HL_NUMBER
+	HL_NUMBER,
+	HL_MATCH
 };
 
 /*** data ***/
@@ -224,6 +225,8 @@ int editorSyntaxToColor(int hl) {
 	switch (hl) {
 		// ANSI foreground red
 		case HL_NUMBER: return 31;
+		// ANSI forground blue
+		case HL_MATCH: return 34;
 		// ANSI foreground white
 		default: return 37;
 	}
@@ -475,6 +478,15 @@ void editorFindCallback(char *query, int key) {
 	static int last_match = -1;
 	static int direction = 1;
 
+	static int saved_hl_line;
+	static char *saved_hl = NULL;
+
+	if (saved_hl) {
+		memcpy(config.row[saved_hl_line].hl, saved_hl, config.row[saved_hl_line].rsize);
+		free(saved_hl);
+		saved_hl = NULL;
+	}
+
 	if (key == '\r' || key == '\x1b') {
 		last_match = -1;
 		direction = 1;
@@ -505,6 +517,11 @@ void editorFindCallback(char *query, int key) {
 			config.cy = current;
 			config.cx = editorRowRxToCx(row, match - row->render);
 			config.rowoff = config.numrows;
+
+			saved_hl_line = current;
+			saved_hl = malloc(row->rsize);
+			memcpy(saved_hl, row->hl, row->rsize);
+			memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
 			break;
 		}
 	}
